d42c7422f4a320cfc38e6fcd826804ebdff80887
Nicolas Wender Fontes Queiroz <nicolas.w3nder@gmail.com>
2023-12-21 13:14:56 -0300

    feat: implementando api e integrando ao front com axios e react-hot-toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import ModalProvider from '@/providers/modal-provider'
+import ToasterProvider from '@/providers/toats-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="pt-br">
         <body className={inter.className}>
+          <ToasterProvider/>
           <ModalProvider />
           {children}
         </body>
--- src/components/modals/store-modal.tsx
+++ src/components/modals/store-modal.tsx
@@ -1,9 +1,10 @@
 'use client'
+
 import * as z from 'zod'
-import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import { useStoreModal } from '@/hooks/use-store-modal'
+
 import Modal from '../ui/modal'
 import {
   Form,
@@ -16,12 +17,20 @@ import {
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
 
+import { useForm } from 'react-hook-form'
+import { useState } from 'react'
+
+import axios from 'axios'
+import toast from 'react-hot-toast'
+
 const formSchema = z.object({
   name: z.string().min(1)
 })
 
 export default function StoreModal() {
   const storeModal = useStoreModal()
+
+  const [loading, setLoading] = useState(false)
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -31,8 +40,15 @@ export default function StoreModal() {
   })
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values)
-    //Create Store
+    try {
+      setLoading(true)
+      await axios.post('/api/stores', values)
+      toast.success('Store created successfully')
+    } catch (error) {
+      toast.error('Something went wrong. Please try again later.')
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
@@ -53,14 +69,22 @@ export default function StoreModal() {
                   <FormItem>
                     <FormLabel>Name</FormLabel>
                     <FormControl>
-                      <Input placeholder="E-Commerce" {...field} />
+                      <Input
+                        disabled={loading}
+                        placeholder="E-Commerce"
+                        {...field}
+                      />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
               <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                <Button variant="outline" onClick={storeModal.onClose}>
+                <Button
+                  disabled={loading}
+                  variant="outline"
+                  onClick={storeModal.onClose}
+                >
                   Cancel
                 </Button>
                 <Button type="submit">Continue</Button>
